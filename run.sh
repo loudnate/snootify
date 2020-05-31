0b703548d22ca20471981a38c302ef1a3c18bc13
Nathan Racklyeft <loudnate@gmail.com>
2020-05-30 23:27:57 -0700

    Scripts

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ python3 -m venv "$HOME/$VENV"
 # Activate the environment. (will also succeed silently if already activated)
 source "$HOME/$VENV/bin/activate"
 
-# install the package and its dependencies
-python3 setup.py install
+# run the default command
+snootify send
