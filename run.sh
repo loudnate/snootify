55480f72b49c89e061ba2dc1282d0816288d7bbc
Nathan Racklyeft <loudnate@gmail.com>
2020-05-30 23:53:50 -0700

    Adding a sample crontab entry

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 
 VENV="snootipy"
 
-# Create the virtual environment (will succeed silently if exists)
-python3 -m venv "$HOME/$VENV"
-
 # Activate the environment. (will also succeed silently if already activated)
 source "$HOME/$VENV/bin/activate"
 
